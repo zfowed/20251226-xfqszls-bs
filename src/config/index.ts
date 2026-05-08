0ea40d4731b204fb712a0a2abc77efa4f214e67b
zfowed <zfowed@qq.com>
2026-05-08 10:01:31 +0800

    Update package dependencies and configuration settings. Upgraded zf-dbs to version 1.1.0, updated lockfile version to 9.0, and modified app configuration to set a fixed app width of 4544.

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,35 +2,15 @@ import { defineAppConfig } from 'zf-dbs'
 
 const global = window as { appConfig?: ReturnType<typeof defineAppConfig> }
 
-// 生成一个包含步长的范围数组
-function rangeWithStep (start: number, end: number, step: number) {
-  if (step <= 0) throw new Error('step 必须是正数')
-
-  const result = []
-  const count = Math.floor(Math.abs(end - start) / step)
-  const dir = start < end ? 1 : -1
-
-  for (let i = 0; i <= count; i++) {
-    result.push(Math.floor(start + dir * step * i))
-  }
-
-  result.push(Math.floor(end))
-  return [...new Set(result)]
-}
-
 const config = defineAppConfig({
   // 项目名称
   appTitle: '可视化大屏',
   // 项目宽度，用于计算缩放比例，如果需要适配多个宽度
-  // appWidth: 9472,
-  // appWidths: [9472, 4544],
-  appWidths: rangeWithStep(9472, 4544, (9472 - 4544) / 50), // 生成 50 个宽度的适配列表
+  appWidth: 4544,
   // 项目高度，用于计算缩放比例（非必要不要修改，通过修改宽度来适配）
   appHeight: 2556,
   // 是否使用缩放
   useScale: true,
-  // 是否拉伸缩放
-  useStretch: false,
   // 是否嵌入到 UE 中
   useUE: false,
   // 轮询间隔
